c301cc0bae3a81849558c549edca703a579af886
Jakob <office@chilimatic.com>
2016-03-03 11:57:01 +0100

    - current state

--- web/js/app/frontend/project/directive.js
+++ web/js/app/frontend/project/directive.js
@@ -5,10 +5,13 @@ define(['app'], function(app) {
         return {
             restrict : 'E',
             scope : {
-                list : "="
+                list : "=",
+                dropTarget : '='
             },
-            template: '<div class="col-xs-4 task-list"><span class="task tag" ng-repeat="task in list" ng-if="task.name">{{task.name}}<a class="glyphicon glyphicon-remove" ng-click="close(task.id)"></a></span></div>',
-            link : function(scope, element, attr) {
+            templateUrl: '/js/app/frontend/header/header-tpl.html',
+            link : function(scope, element, attr)
+            {
+
                 scope.close = function(selectedId)
                 {
                     for (var i in scope.list) {
